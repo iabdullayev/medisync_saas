0a53bbdee8b8fe623504f49a38c8aec814fd6bc2
nabdull2 <nabdull2@ncsu.edu>
2025-12-25 13:28:01 -0500

    infra: enable cloudfront & remove sensitive tfstate from git

--- infra/main.tf
+++ infra/main.tf
@@ -178,21 +178,20 @@ resource "aws_route53_zone" "public" {
   name = var.root_domain
 }
 
-# Root Domain (ALIAS to S3 Redirect)
+# Root Domain (ALIAS to CloudFront for HTTPS support)
 resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.public.zone_id
   name    = var.root_domain
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.root_redirect.website_domain
-    zone_id                = aws_s3_bucket.root_redirect.hosted_zone_id
+    name                   = aws_cloudfront_distribution.root_redirect.domain_name
+    zone_id                = aws_cloudfront_distribution.root_redirect.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-# Note: The 'www' record is intentionally NOT managed here so you can 
-# point it to Streamlit Cloud manually in the AWS Console (CNAME).
+# Note: The 'www' record is intentionally NOT managed here (Manual CNAME to Streamlit)
 
 ########################
 # ACM & API Config (Backend)
@@ -205,6 +204,99 @@ resource "aws_acm_certificate" "api" {
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+########################
+# ACM for Root Domain (Needed for CloudFront)
+########################
+
+resource "aws_acm_certificate" "root" {
+  domain_name       = var.root_domain
+  validation_method = "DNS"
+  provider          = aws.us-east-1
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "root_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.root.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.public.zone_id
+}
+
+resource "aws_acm_certificate_validation" "root" {
+  provider                = aws.us-east-1
+  certificate_arn         = aws_acm_certificate.root.arn
+  validation_record_fqdns = [for record in aws_route53_record.root_validation : record.fqdn]
+}
+
+########################
+# CloudFront for Root Redirect (HTTPS -> WWW)
+########################
+
+resource "aws_cloudfront_distribution" "root_redirect" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Root Domain Redirect (HTTPS support)"
+  aliases             = [var.root_domain]
+  price_class         = "PriceClass_100" # Lowest cost (US/EU only)
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.root_redirect.website_endpoint
+    origin_id   = "S3Redirect"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only" # S3 Website only speaks HTTP
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3Redirect"
+
+    # Forward nothing, just cache the 301 redirect
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.root.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
 
